5cf5c6f8a81782c9728e41af4c0896c78c1188c0
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-01-05 00:26:53 +0100

    lots of changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,215 @@ import React from 'react'
 export default function home() {
     
   return (
-   <div><h1>HOME</h1>Home works</div>
+    <div className='home-container'>
+      <div className='top-title'>
+        <h1>Historia</h1>
+
+      </div>
+
+      <div className='info-home'>
+        <div className='top'>
+          <hr></hr>
+          <div className='flex-hej'>
+          <div>
+            <table className='winners'>
+            <li><p className='title-top'>Draftligan 2020-2021</p></li>
+              <tr><td><i class="fas fa-trophy"></i> 1.    Wille - A Team of Carraghers</td>1786</tr>
+              <tr><td>2. Micke - Källströms Kabaré</td>1681</tr>
+              <tr><td>3. Lasse - Villains</td>1679p</tr>
+              <tr><td>4. Joel - BK Slaggberget</td>1648p</tr>
+              <tr><td>5. Lofsen  Mini-Spurs</td>1465p</tr>
+              <tr><td>6. Tompa - Plopparna</td>1418p</tr>
+              <tr><td>7. Björn - FC Premium</td>1275p</tr>
+              <tr><td>8. Forsen - IFK Måsfobi</td>1123p</tr>
+            </table >
+            </div>
+            <div className='year'>
+              <p className='title-top-right'>Årets målvakt</p>
+              <p className='semi-title'>Emiliano Martinez, Villains</p>
+              <p className='description'>Stor anledning till både Villains och Villas fina säsonger. Poängbäst bland målvakterna.</p>
+              <p className='title-top-right'>Årets försvarare</p>
+              <p className='semi-title'>Rúben Dias, Villans</p>
+              <p className='description'>Inte poängbäst men bäst. Tog sig in på topp 6 som mittback och efter att ha anslutit sig inför omgång 4.</p>
+              <p className='title-top-right'>Årets mittfältare</p>
+              <p className='semi-title'>Bruno Fernandes, AToC</p>
+              <p className='description'>Välförtjänt dubbad Penandes. Frågan är hur mycket den som valde TAA före ångrar det.</p>
+              <p className='title-top-right'>Årets anfallare</p>
+              <p className='semi-title'>Harry Kane, Mini-Spurs</p>
+              <p className='description'>Vinnare av både mål och assistligan, sensationellt med tanke på vilka han spelar för. </p>
+              <p className='title-top-right'>Årets fynd</p>
+              <p className='semi-title'>Mason Mount, Villains</p>
+              <p className='description'>Vald efter Enda Stevens, bara en sådan sak... Hade varit Jack Harrison om hans tränare trott mer på honom.</p>
+              <p className='title-top-right'>Årets flopp</p>
+              <p className='semi-title'>Donny Van De Beek</p>
+              <p className='description'>Kaxigt dubbad som årets fynd inför ligans start av tränaren Björn Gabrielsson, även tidigt dumpad och samladede ihop totalt 32 poäng.</p>
+              <p className='title-top-right'>Årets mygelspelare</p>
+              <p className='semi-title'>Stuart Dallas</p>
+              <p className='description'>Hos samma manager för andra året i följd, något luktar fisk... Blev poängbästa försvarare med den lilla asterisken att han spelade hela säsongen på mittfältet.</p>
+            </div>
+            <div className='year'>
+
+              <p className='title-top-right'>Årets va, har han inte fått sparken?</p>
+              <p className='semi-title'>Fredrik Forslin, IFK Måsfobi</p>
+              <p className='description'>Visade mindre intresse än Christian Eriksen tills han hotades med uteslutning av ligans president och lyckades tillsammans med osedvanligt flyt och spelare som tidigare knappt fick spela med b-laget som hittade formen under coronapandemin få ihop några hyggliga omgångar. Ändå rättvist tvärsist.</p>
+              <p className='title-top-right'>Årets förbättringspotential</p>
+              <p className='semi-title'>Fredrik Forslin, IFK Måsfobi</p>
+              <p className='description'>Kanske den mest konsekventa. spikrak formkurva i form av slutplacering. Ska bli spännande att se om han kan slå sig egen total när han tvingats lämna in startelvor.</p>
+              <p className='title-top-right'>Årets kohandlare</p>
+              <p className='semi-title'>Wilhelm Groth, AToC</p>
+              <p className='description'>Regeltolkaren nummer 1. Lyckades med konststycket att byta bort alla sina spelare förutom en. Richarlison slutade nog lära känna sina nya lagkamrater efter ett tag.</p>
+              <p className='title-top-right'>Årets FPL-spelare</p>
+              <p className='semi-title'>Bruno Fernadndes, AToC</p>
+              <p className='description'>Poängbäst i ligan och smått genial. Fick avsluta säsongen i rätt färger och betalade tillbaka genom att vila.</p>
+              <p className='title-top-right'>Årets manager</p>
+              <p className='semi-title'>Wilhelm Groth, AToC</p>
+              <p className='description'>Visade att lojalitet är överskattat, största segermarginalen i ligans korta historia.</p>
+            </div>
+          </div>
+
+          
+          <hr></hr>
+          <div className='flex-hej'>
+            <div>
+            <table className='winners'>
+            <li><p className='title-top'>Draftligan 2019-2020</p></li>
+              <tr><td><i class="fas fa-trophy"></i> 1.  Björn FC Premium</td> 1566 </tr>
+              <tr><td>2. Tompa  Plopparna</td>   1542p</tr>
+              <tr><td>3. Wille  A Team of Carraghers</td>1526p</tr>
+              <tr><td>4. Micke  Fyrklöverns FK</td> 1477p</tr>
+              <tr><td>5. Lofsen  Mini-Spurs</td>  1469p</tr>
+              <tr><td>6. Joel   BK Slaggberget</td>1463p</tr>
+              <tr><td>7. Lars  Villains   </td>1259</tr>
+              <tr><td>8. Forsen  IFK Måsfobi </td></tr>
+            </table >
+            </div>
+            <div className='year'>
+              <p className='title-top-right'>Årets målvakt</p>
+              <p className='semi-title'>Nick Pope, BK Slaggberget</p>
+              <p className='description'>Vald sist av alla spelare och hade förutom flest poäng av alla målvakter 15 hållna nollor.</p>
+              <p className='title-top-right'>Årets försvarare</p>
+              <p className='semi-title'>Trent Alexander-Arnold</p>
+              <p className='description'>Förhandsfavorit som höll för trycket, 14 nollor, 15 assist och 4 mål.</p>
+              <p className='title-top-right'>Årets mittfältare</p>
+              <p className='semi-title'>Kevin De Bruyne</p>
+              <p className='description'>13 mål och 23 assist, vilket är vinst av Premier leagues poängliga.</p>
+              <p className='title-top-right'>Årets anfallare</p>
+              <p className='semi-title'>Jamie Vardy, Plopparna</p>
+              <p className='description'>23 mål och ensam vinnare av skytteligan före mer namnkunniga spelare.</p>
+              <p className='title-top-right'>Årets fynd</p>
+              <p className='semi-title'>Danny Ings, Plopparna</p>
+              <p className='description'>Inte ens vald i draften men springer ändå in 22 bollar i nät samtidigt som hans medspelare gjorde sitt bästa för att åka ur.</p>
+              <p className='title-top-right'>Årets flopp</p>
+              <p className='semi-title'>Christian Eriksen, Fyrklöverns FK</p>
+              <p className='description'>Valdes tidigt men visade sig helt ointresserad av att spela och sattes på ett plan till Italien.</p>
+            </div>
+            <div className='year'>
+              <p className='title-top-right'>Årets upp som en sol en som en pannkaka"</p>
+              <p className='semi-title'>Teemu Pukki, FC Premium</p>
+              <p className='description'>Gav sin manager svår hybris i säsongsinledningen innan även han själv också kom ihåg att han var finländare och dålig.</p>
+              <p className='title-top-right'>Årets va, har han inte fått sparken?</p>
+              <p className='semi-title'>Fredrik Forslin, IFK Måsfobi</p>
+              <p className='description'>Visade mindre intresse än Christian Eriksen tills han hotades med uteslutning av ligans president och lyckades tillsammans med osedvanligt flyt och spelare som tidigare knappt fick spela med b-laget som hittade formen under coronapandemin få ihop några hyggliga omgångar. Ändå rättvist tvärsist.</p>
+              <p className='title-top-right'>Årets regeltolkare</p>
+              <p className='description'> className='semi-title'Wilhelm Groth, A Team of Carraghers</p>
+              <p className='description'>Manipulerade, smörade, skämtade, hotade, argumenterade och bytte som en vanvettig. Kanske sin egen värsta fiende.</p>
+              <p className='title-top-right'>Årets spelare</p>
+              <p className='semi-title'>Kevin De Bruyne, FC Premium</p>
+              <p className='description'>Fick alla som heter Jordan Hendersson att se ut som en dålig variant av en trasig krona. Målskytt, framspelare, pådrivare och allmänt fotbollsgeni. </p>
+            </div>
+          </div>          
+
+          <hr></hr>
+          <div className='flex-hej'>
+            <table className='winners'>
+              <li><p className='title-top'>Draftligan 2018-2019</p></li>
+              <tr><td>1. Micke - Fyrklöverns FK</td> 1596p </tr>
+              <tr><td>2. Joel - BK Slaggberget</td>   1530p</tr>
+              <tr><td>3. Björn - FC Premium</td>   1499p</tr>
+              <tr><td>4. Tompa - Plopparna   </td> 1454p</tr>
+              <tr><td>5. Lofsen - Mini-Spurs</td>  1395p</tr>
+              <tr><td>6. Wille - A Team of Carraghers  </td>  1386p</tr>
+              <tr><td>7. Lars - Villains   </td>1384p</tr>
+              <tr><td>8. Forsen - IFK Måsfobi </td>  1337p</tr>
+            </table >
+
+            <div className='year'>
+              <p className='title-top-right'>Årets målvakt</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets försvarare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets mittfältare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets anfalallare</p>
+              <p>Ingen data</p>
+            </div>
+            <div className='year'>
+              <p className='title-top-right'>Årets mygelspelare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets manager</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets besvikelse</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets flopp</p>
+              <p>Ingen data</p>
+            </div>
+          </div>
+
+         <hr></hr>
+          <div className='flex-hej'>
+            <table className='winners'>
+              <li><p className='title-top'>Draftligan 2017-2018</p></li>
+              <tr><td><i class="fas fa-trophy"></i> 1. Wille - Ziemligh Gut</td> 1226p </tr>
+              <tr><td>2. Joel - BK Slaggberget</td>   1175p</tr>
+              <tr><td>3. Lofsen - Mini-Spurs</td>   1047p</tr>
+              <tr><td>4. Lars - Villains   </td> 1032p</tr>
+              <tr><td>5. Björn - FC Premium  </td>  975p</tr>
+              <tr><td>6. Forsen - FK Måsfobi  </td>  942p</tr>
+              <tr><td>7. Tompa - Plopparna   </td>927p</tr>
+              <tr><td>8. Micke - Ett kraftigt jävla gäng </td>  844p</tr>
+              <tr><td>9. Niklas - Bortglömda Brasilien </td>734p</tr>
+            </table >
+            <div className='year'>
+              <p className='title-top-right'>Årets målvakt</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets försvarare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets mittfältare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets anfallare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets flopp</p>
+              <p>Ingen data</p>
+
+            </div>
+            <div className='year'>
+              <p className='title-top-right'>Årets manager</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets förbättringspotential</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets regeltolkare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets spelare</p>
+              <p>Ingen data</p>
+              <p className='title-top-right'>Årets fynd</p>
+              <p>Ingen data</p>
+
+            </div>
+          </div>
+
+    
+
+
+      
+        </div>
+
+
+      </div>
+      <hr></hr>
+    </div>
+
+
+
   )
 }
 
