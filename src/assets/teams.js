3f9f84013d4741a7b568a626ed650fbb90b8d121
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-01-06 00:45:24 +0100

    tewst

--- src/assets/teams.js
+++ src/assets/teams.js
@@ -25,7 +25,6 @@ export const getTeams = [
         teamname: "Plopparna",
         firstName: "Tommy",
         lastName: " Forslin",
-        icon: "fas fa-tshirt",
         img: "https://scontent-arn2-2.xx.fbcdn.net/v/t1.18169-9/935208_10201312750441188_1215249357_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ei5vpnt6ozIAX9UYQXK&_nc_ht=scontent-arn2-2.xx&oh=00_AfCGQKmO2UvQm_q-XPG-swoiXqMcVB8qTi1xApVXgKP_uA&oe=63D4CBE6",
         lineup:
         {
@@ -44,7 +43,6 @@ export const getTeams = [
         firstName: "Wilhelm",
         lastName: "Groth",
         img: "https://media.licdn.com/dms/image/D4D03AQHFf2e4hlKQsw/profile-displayphoto-shrink_800_800/0/1669793398317?e=1678320000&v=beta&t=O_2jkTUmAPlWAK4-lLjWhu4Lm4CX9iUnNa5gCwFT260",
-        icon: "fas fa-tshirt",
         lineup:
         {
             "goalkeeper": "Kepa Arrizabalaga",
@@ -63,7 +61,6 @@ export const getTeams = [
         firstName: "Björn",
         lastName: "Gabrielsson",
         img: "https://scontent-arn2-1.xx.fbcdn.net/v/t39.30808-6/302487462_2481746258634365_5148902375641916711_n.jpg?stp=cp6_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=y9OzSeVGWhcAX8ovgK7&_nc_ht=scontent-arn2-1.xx&oh=00_AfBauHhDreVLogZn9RP2padE4mW1qkN99r0ULhziYoWCnA&oe=63B143D2",
-        icon: "fas fa-tshirt",
         lineup:
         {
             "goalkeeper": "Ederson Moraes",
@@ -117,8 +114,8 @@ export const getTeams = [
     {
         teamname: "Villains",
         firstName: "Lars",
-        img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/165228_1261453912814_779331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=vxNRks_-dtUAX_Vlz4V&tn=pImbR-kqUMD591Zb&_nc_ht=scontent-arn2-1.xx&oh=00_AfC3-P-n9np6qzK2Nz3Am0H3XhGVByBlyZhRfr2AMpLhVA&oe=63D4D256",
         lastName: "Berglund",   
+        img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/165228_1261453912814_779331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=vxNRks_-dtUAX_Vlz4V&tn=pImbR-kqUMD591Zb&_nc_ht=scontent-arn2-1.xx&oh=00_AfC3-P-n9np6qzK2Nz3Am0H3XhGVByBlyZhRfr2AMpLhVA&oe=63D4D256",
         lineup:
         {
             "goalkeeper": "Robin Olsen",
@@ -135,8 +132,8 @@ export const getTeams = [
     {
         teamname: "Källströms Kabaré",
         firstName: "Mikael",
-        img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/165228_1261453912814_779331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=vxNRks_-dtUAX_Vlz4V&tn=pImbR-kqUMD591Zb&_nc_ht=scontent-arn2-1.xx&oh=00_AfC3-P-n9np6qzK2Nz3Am0H3XhGVByBlyZhRfr2AMpLhVA&oe=63D4D256",
         lastName: "Andersson",
+        img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/165228_1261453912814_779331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=vxNRks_-dtUAX_Vlz4V&tn=pImbR-kqUMD591Zb&_nc_ht=scontent-arn2-1.xx&oh=00_AfC3-P-n9np6qzK2Nz3Am0H3XhGVByBlyZhRfr2AMpLhVA&oe=63D4D256",
         lineup:
         {
             "goalkeeper": "Alisson Becker",
