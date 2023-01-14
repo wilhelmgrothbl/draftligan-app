bf43d2af3033fd475cf6fc196382c9fa6834fd3e
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-01-14 22:49:46 +0100

    update score

--- src/assets/database.js
+++ src/assets/database.js
@@ -5,49 +5,49 @@ export const gameweek = {
 export const Standings = [
     {
         name: "Wilhelm Groth",
-        score: 643,
+        score: 685,
         team: "A team of Carraghers",
         img: "https://media.licdn.com/dms/image/D4D03AQHFf2e4hlKQsw/profile-displayphoto-shrink_800_800/0/1669793398317?e=1678320000&v=beta&t=O_2jkTUmAPlWAK4-lLjWhu4Lm4CX9iUnNa5gCwFT260"
     }, 
     {
         name: "Björn Gabrielsson", 
-        score: 662, 
+        score: 713, 
         team: "FC Premium", 
         img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.15752-9/318615647_2996241000682006_7276209106184566331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=S6ciW_klfy4AX-8DF5S&_nc_ht=scontent-arn2-1.xx&oh=03_AdSfUijV95PJeITB-lQ_RcnYkXtKAUjIQ25A8pmC2MNcpA&oe=63DECA7A",
     },
     {
         name: "Fredrik Torin", 
-        score: 676, 
+        score: 734, 
         team: "Homecoming Queens FC", 
         img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/85014130_10157496938492912_8699274705485430784_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=yCxU0Vk-YBgAX8Oyvqo&_nc_ht=scontent-arn2-1.xx&oh=00_AfAuXRZmKowcT5l0XbWqT_L_2iAkKzW1tvN2UPu9Z07wnw&oe=63D41252",
     },
     {
         name: "Joel Nordqvist", 
-        score: 640,
+        score: 676,
         team: "BK Slaggberget", 
         img: "https://scontent-arn2-1.xx.fbcdn.net/v/t39.30808-6/281528244_5268566136534519_2577984506836793189_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=83y_mupX1vYAX9wnXC0&_nc_ht=scontent-arn2-1.xx&oh=00_AfD1rAEFmr13z-8qVQNBu5gi3aNsfsD7O_1B3OT7J634XQ&oe=63BD144B",
     },
     {
         name: "Tommy Forslin", 
-        score: 735,
+        score: 773,
         team: "Plopparna", 
         img: "https://scontent.xx.fbcdn.net/v/t1.15752-9/318528210_1424218431443256_9040277765076907010_n.jpg?stp=dst-jpg_p206x206&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_ohc=K8Ro5sroPA0AX8s6kHS&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSdlMM08x0dvYwfUEUkJ8HVh7ycdwopogpvX4gwgpJGiA&oe=63DEE74F",
     },
     {
         name: "Magnus Olofsson", 
-        score: 688,
+        score: 724,
         team: "Mini-Spurs", 
         img: "https://scontent-arn2-2.xx.fbcdn.net/v/t1.18169-9/935208_10201312750441188_1215249357_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ei5vpnt6ozIAX9UYQXK&_nc_ht=scontent-arn2-2.xx&oh=00_AfCGQKmO2UvQm_q-XPG-swoiXqMcVB8qTi1xApVXgKP_uA&oe=63D4CBE6",
     },
     {
         name: "Lars Berglund", 
-        score: 619,
+        score: 654,
         team: "Villains", 
         img: "https://scontent-arn2-1.xx.fbcdn.net/v/t1.18169-9/165228_1261453912814_779331_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=vxNRks_-dtUAX_Vlz4V&tn=pImbR-kqUMD591Zb&_nc_ht=scontent-arn2-1.xx&oh=00_AfC3-P-n9np6qzK2Nz3Am0H3XhGVByBlyZhRfr2AMpLhVA&oe=63D4D256",
     },
     {
         name: "Mikael Andersson", 
-        score: 602,
+        score: 660,
         team: "Källströms Kabaré", 
         img: "https://www.iggesundsik.se/images/172/3312/_user19059_1.jpg",
     }
