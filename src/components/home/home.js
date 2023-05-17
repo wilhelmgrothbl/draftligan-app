39db0e500fad8ff5b4ca28aaceea736659c8dd99
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-05-17 15:41:32 +0200

    dddd

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -61,45 +61,49 @@ const people = [
 ];
 
 export default function Home() {
-    return (
-      <div className="bg-gradient-to-r from-indigo-900 to-red-500 py-8 sm:py-16">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="flex flex-col items-center">
+  return (
+    <div className="bg-gradient-to-r from-indigo-900 to-red-500 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 flex items-center">
+          <div className="mr-6">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              De efterkloka
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-white">
+              De Efterkloka: Kamratföreningen som balanserar klokhet och humor
+              sedan 2016 - skämtsamt visdomsfulla med en touch av glimten i
+              ögat!
+            </p>
+          </div>
+          <div className="flex-shrink-0">
             <img
-              className="h-48 sm:h-64 mb-4"
+              className="h-64"
               src="./././efterkloka.png"
               alt="här var det en ko"
             />
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl text-center">
-              De efterkloka
-            </h2>
-            <p className="mt-4 text-lg leading-7 text-white text-center">
-              De Efterkloka: Kamratföreningen som balanserar klokhet och humor sedan 2016 - skämtsamt visdomsfulla med en touch av glimten i ögat!
-            </p>
           </div>
-  
-          <ul
-            role="list"
-            className="mx-auto mt-12 grid max-w-2xl grid-cols-1 gap-y-8 sm:grid-cols-2 sm:max-w-none"
-          >
-            {people.map((person) => (
-              <li key={person.name} className="flex flex-col items-center">
-                <img
-                  className="aspect-[3/2] w-full rounded-2xl object-cover"
-                  src={person.imageUrl}
-                  alt=""
-                />
-                <h3 className="mt-4 text-lg font-semibold leading-7 text-white text-center">
-                  {person.name}
-                </h3>
-                <p className="mt-1 text-base leading-6 text-white text-center">
-                  {person.role}
-                </p>
-              </li>
-            ))}
-          </ul>
         </div>
+
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-3 gap-x-8 gap-y-16 sm:grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-4"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <img
+                className="aspect-[3/2] w-full rounded-2xl object-cover"
+                src={person.imageUrl}
+                alt=""
+              />
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">
+                {person.name}
+              </h3>
+              <p className="text-base leading-7 text-white">{person.role}</p>
+        
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
