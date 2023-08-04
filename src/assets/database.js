a3fbfe7b1fdfef4064b17a7e611e8209b9bae8c1
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-08-04 07:40:26 +0200

    data

--- src/assets/database.js
+++ src/assets/database.js
@@ -1,57 +1,54 @@
 export const gameweek = {
-    gw: "38"
+    gw: "1"
 }
 
 export const Standings = [
     {
         name: "Wilhelm Groth",
-        score: 1613,
+        score: 0,
         team: "A team of Carraghers",
         img: "././wille.jpg"
     }, 
     {
         name: "Björn Gabrielsson", 
-        score: 1395, 
+        score: 0, 
         team: "FC Premium", 
         img: "././bjorn.jpg"
     },
     {
         name: "Fredrik Torin", 
-        score: 1451, 
+        score: 0, 
         team: "Homecoming Queens FC", 
         img: "././forsen.jpg",
     },
     {
         name: "Joel Nordkvist", 
-        score: 1528,
+        score: 0,
         team: "BK Slaggberget", 
         img: "././joel.jpg",
     },
     {
         name: "Tommy Forslin", 
-        score: 1645,
+        score: 0,
         team: "Plopparna", 
         img: "././tmy.jpg",
     },
     {
         name: "Magnus Olofsson", 
-        score: 1477,
+        score: 0,
         team: "Mini-Spurs", 
         img: "././lofsen.jpg",
     },
     {
         name: "Lars Berglund", 
-        score: 1497,
+        score: 0,
         team: "Villains", 
         img: "././lasse.jpg",
     },
     {
         name: "Mikael Andersson", 
-        score: 1407,
+        score: 0,
         team: "Källströms Kabaré", 
         img: "././micke.jpg",
     }
-
-
-
 ]
